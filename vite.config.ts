5992e433e2844752972129e16f9ba588c038aabd
Sitnikov Timofey <t.sitnikov@infocom-s.ru>
2025-05-06 16:50:40 +0300

    исправил отступы

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgo from 'vite-plugin-svgo'
 
 export default defineConfig({
-  plugins: [
-    svgo(), // Оптимизация SVG
-    ViteImageOptimizer({
-      // Настройки преобразования в WebP
-      webp: {
-        quality: 80,
-        lossless: false,
-      },
-      // Генерация разных размеров
-      includePublic: true,
-      logStats: true,
-    }),
-  ]
+    plugins: [
+        svgo(), // Оптимизация SVG
+        ViteImageOptimizer({
+            // Настройки преобразования в WebP
+            webp: {
+                quality: 80,
+                lossless: false,
+            },
+            // Генерация разных размеров
+            includePublic: true,
+            logStats: true,
+        }),
+    ]
 })
